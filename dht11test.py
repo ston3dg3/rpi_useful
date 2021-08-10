47aa44b316c7c03460cf7ef9aa5bdbac24566e62
ston3dg3 <87919864+ston3dg3@users.noreply.github.com>
2021-08-11 00:13:43 +0200

    comments on the dht11 file

--- dht11test.py
+++ dht11test.py
@@ -1,10 +1,16 @@
+# Python Script: dht11test.py
+# Purpose: print temperature and humidity from dht11 sensor to the console for testing 
+# -------------------------------
+
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
 
 
 sensor = Adafruit_DHT.DHT11
+# select the right GPIO pin
 pin = 17
+# choose how frequently data is updated 
 sleep_time = 2.0
 
 #GPIO setup
