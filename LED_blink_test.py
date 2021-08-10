59ad68223951ebec4345fa2971c17ff5dcf10bae
ston3dg3 <87919864+ston3dg3@users.noreply.github.com>
2021-08-11 00:15:50 +0200

    Update Led_blink_test.py with comments

--- LED_blink_test.py
+++ LED_blink_test.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+# Script: LED_blink_test.sh
+# Purpose: turn LED at GPIO pin 22 on and off after a short delay
+# sets HIGH to pin 22 for one second then low for 1 second
+# -------------------------------
+
 import RPi.GPIO as GPIO
 import time
 
